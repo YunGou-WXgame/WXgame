10075cf29f36f82e4cc22d1448001030ab9a05b8
YunGou112 <2466984288@qq.com>
2025-08-13 22:04:14 +0800

    第三次修改

--- WXgame/urls.py
+++ WXgame/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.views.static import serve
+from django.conf import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    # path("wxgameapp/", include("wx_game_app.urls")),
+    path('media/<path:path>', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
